d1326543b9b116d6c0905faee7d59846ec75614b
yariksid <33714364+yariksid@users.noreply.github.com>
2019-06-04 12:42:10 +0900

    Add files via upload

---
+++ blog/get.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="GET" action="result.php">
+		<input type="" name="index">
+		<button>Click!</button>
+	</form>
+</body>
+</html>
